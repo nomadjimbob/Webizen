f43f38be33a943924ef221432083c8c563c56cc6
Bear Riley <46008563+BehrRiley@users.noreply.github.com>
2020-07-18 12:23:20 -0400

    Added import for POST method

--- src/main/java/space/morphanone/webizen/server/WebServer.java
+++ src/main/java/space/morphanone/webizen/server/WebServer.java
@@ -4,6 +4,7 @@ import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 import com.sun.net.httpserver.HttpServer;
 import space.morphanone.webizen.events.GetRequestScriptEvent;
+import space.morphanone.webizen.events.PostRequestScriptEvent;
 
 import java.io.IOException;
 import java.net.InetSocketAddress;
