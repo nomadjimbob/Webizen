8130d5227c9dc9476c25a9e5e8b07b6285fde705
Ph4i1ur3 <Ph4i1ur3@yahoo.com>
2020-08-29 17:45:01 -0400

    java starts at 0....

--- src/main/java/space/morphanone/webizen/events/PostRequestScriptEvent.java
+++ src/main/java/space/morphanone/webizen/events/PostRequestScriptEvent.java
@@ -126,7 +126,7 @@ public class PostRequestScriptEvent extends BasicRequestScriptEvent {
             MapTag map = new MapTag();
             Headers headers = httpExchange.getRequestHeaders();
             for (String key : headers.keySet()) {
-                map.putObject(key, new ElementTag(headers.get(key).get(1)));
+                map.putObject(key, new ElementTag(headers.get(key).get(0)));
             }
             return map;
         }
